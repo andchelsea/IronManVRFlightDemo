7b3016e7f31faaf7c17c45ffcfa556c1065c36eb
shyguy16 <eashy16@gmail.com>
2017-05-09 12:33:26 -0700

    ammo and health ui update

---
+++ SeniorProject/Assets/AmmoManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class AmmoManager : MonoBehaviour
+{
+    public static AmmoManager Instance;
+    private Text Ammo;
+    // Use this for initialization
+    void Start()
+    {
+        Ammo = GetComponent<Text>();
+    }
+    void Awake()
+    {
+        if (Instance != null && Instance != this)
+            Destroy(this.gameObject);
+        Instance = this;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void AmmoUpdate()
+    {
+        Ammo.text = VrPlayer.Instance.Ammo.ToString();
+    }
+}
---
+++ SeniorProject/Assets/Scripts/HealthManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class HealthManager : MonoBehaviour {
+
+    public static HealthManager Instance;
+    private Text health;
+    // Use this for initialization
+    void Start ()
+    {
+        health = GetComponent<Text>();
+       
+	}
+    void Awake()
+    {
+        if (Instance != null && Instance != this)
+            Destroy(this.gameObject);
+        Instance = this;
+    }
+    // Update is called once per frame
+    void Update ()
+        {
+            
+        }
+
+    void Damaged()
+    {
+        health.text = VrPlayer.Instance.GetHealth().ToString();
+    }
+}
