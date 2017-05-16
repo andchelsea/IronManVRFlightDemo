aa8a4a24ff81223e4e4b7a3ad0d07b7e9e2a75e9
andchelsea <andchelsea@gmail.com>
2017-05-16 12:25:50 -0700

    Flare + Enemy Interaction Updates
    Flare tag created, used to detect an ontrigger collision with enemy. If
    triggered will show VR player the enemy. Need to still fix on mouse
    over/exit function and add flare tag to flare 2

--- SeniorProject/Assets/Scripts/EnemyScript.cs
+++ SeniorProject/Assets/Scripts/EnemyScript.cs
@@ -4,6 +4,10 @@ using System.Collections;
 public class EnemyScript : MonoBehaviour
 {
     private float Lifetime = 3;//needed?
+    private float ChildLife = 0;
+    [SerializeField]
+    private GameObject child;
+    [SerializeField] private float hologramLife = 2;
     [SerializeField] private float life = 0;
     [SerializeField] private Shader unlit;//make a shader
     [SerializeField] private Shader lit;//make a shader
@@ -16,6 +20,8 @@ public class EnemyScript : MonoBehaviour
         Lifetime = life;
         PlayerPos = VrPlayer.Instance.gameObject.transform;
         rb = GetComponent<Rigidbody>();
+        child = this.gameObject.transform.GetChild(0).gameObject;//enemy structure dependednt
+        child.SetActive(false);
     }
 
 	public void Reset()
@@ -30,9 +36,22 @@ public class EnemyScript : MonoBehaviour
     {
         if (Manager.Instance.IsUpdatable())
         { 
+            if(child.activeSelf)
+            {
+                ChildLife -= Time.deltaTime;
+                if (ChildLife < 0.0f)
+                {
+                   child.SetActive(false);//capture the child in awake
+                }
+            }
+
+
             Lifetime -= Time.deltaTime;
             if (Lifetime < 0.0f)
-                this.gameObject.SetActive(false);//will this work??
+            {
+                child.SetActive(false);
+                this.gameObject.SetActive(false);
+            }
 
             transform.LookAt(PlayerPos);
             rb.velocity = rb.velocity.magnitude * transform.forward;
@@ -58,25 +77,25 @@ public class EnemyScript : MonoBehaviour
     {
         if (other.gameObject.tag == "Player")
         {
+            child.SetActive(false);
             this.gameObject.SetActive(false);
-     
 
             if (--VrPlayer.Instance.Health <= 0)
             {
                 Manager.Instance.SetUpdatable(false);
             }
         }
-    
         else if(other.gameObject.tag == "Bullet")
         {
-            this.gameObject.SetActive(false);
+            child.SetActive(false);
             other.gameObject.SetActive(false);
+            this.gameObject.SetActive(false);
+        }
+        else if(other.gameObject.tag == "Flare")
+        {
+            child.SetActive(true);
+            ChildLife = hologramLife;
+            Debug.Log("Enemy hit a Flare!");
         }
     }
-     
-    void OnFlareDetection()
-    {
-        //this.GetComponentInChildren
-    }
-
 }
