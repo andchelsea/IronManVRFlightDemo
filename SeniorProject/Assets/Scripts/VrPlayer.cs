479a45842f933f04aedb3b40705ff545f61ba825
Felix-ShannonChelsea Vict <andchelsea@gmail.com>
2017-06-13 09:12:54 -0700

    Commented the vr CODE

--- SeniorProject/Assets/Scripts/VrPlayer.cs
+++ SeniorProject/Assets/Scripts/VrPlayer.cs
@@ -5,27 +5,33 @@ public class VrPlayer : MonoBehaviour {
 
     public static VrPlayer Instance;
 
-    [SerializeField] public Rigidbody Rb;//probably head/torso, needed to apply velocity
-
-    [Space(10)]
-    [SerializeField] public float FlySpeed = 15; //arbitrary starting numbers, playtest
-    [SerializeField] public float MaxSpeed = 150; //arbitrary starting numbers, playtest
-    [SerializeField] public float ProjectileSpeed = 15; //arbitrary starting numbers, playtest
+    //Variables
+    [SerializeField] public Rigidbody Rb;
     [SerializeField] public float AngleOffset = 90;
 
     [Space(10)]
+    //Flying Variables
+    [SerializeField] public float FlySpeed = 15; 
+    [SerializeField] public float MaxSpeed = 150; 
+
+    [Space(10)]
+    //Gravity Manipulation
     [SerializeField] public float lowGrav = 0.5f;
     [SerializeField] public float highGrav= 1.0f;
     private float gravMultiplier= 1.0f;
 
     [Space(10)]
-    [SerializeField] public int Ammo = 10; //arbitrary starting numbers, playtest
-    [SerializeField] private float AttackCoolDown = 0.5f; //arbitrary starting numbers, playtest
-    [SerializeField] public int Health = 10; //arbitrary starting numbers, playtest
-
-    private int startHealth;
+    //Shooting Variables
+    [SerializeField] public float ProjectileSpeed = 15; 
+    [SerializeField] public int Ammo = 10; 
+    [SerializeField] private float AttackCoolDown = 0.5f;
     private int startAmmo;
-   // [SerializeField] private int NumAmmo = 10; //arbitrary starting numbers, playtest
+
+    //Health Variables
+    [SerializeField] public int Health = 10;
+    private int startHealth;
+
+    //Basic Singleton
     void Awake ()
     {
         if (Instance != null && Instance != this)
@@ -34,19 +40,23 @@ public class VrPlayer : MonoBehaviour {
             return;
         }
         Instance = this;
+
         gravMultiplier = highGrav;
         startHealth = Health;
         startAmmo = Ammo;
     }
 
+   //Reset Game 
    public void Reset()
     {
         this.transform.position.Set(0, 0.6f, -5);
         Health = startHealth;
         Ammo = startAmmo;
     }
+
     public int GetHealth() { return Health; }
 
+    //Check if VR Player can shoot
     public bool Shootable(float AttackDelay)
     {
         if (Ammo > 0 && AttackDelay > AttackCoolDown && Manager.Instance.IsUpdatable())
@@ -61,29 +71,25 @@ public class VrPlayer : MonoBehaviour {
 
     void OnCollisionEnter(Collision other)
     {
-        if (other.collider.tag == "Land")
+        if (other.collider.tag == "Land")//Make it harder to take off
         {
             gravMultiplier = highGrav;
         }
-        //may need to be on the hands (and body?) for ammo
     }
     void OnCollisionExit(Collision other)
     {
-        if (other.collider.tag == "Land")
+        if (other.collider.tag == "Land")//Make it easier to fly
         {
             gravMultiplier = lowGrav;
         }
     }
-
-    // Use this for initialization
 
 	// Update is called once per frame
 	void Update ()
     {
         if (Manager.Instance.IsUpdatable())
         {
-            //AttackDelay += Time.deltaTime;
-            Rb.AddForce(Physics.gravity*gravMultiplier, ForceMode.Force);
+            Rb.AddForce(Physics.gravity*gravMultiplier, ForceMode.Force);//Apply gravity
         }
     }
 }
