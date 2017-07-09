2cf90de3b7c400d7e62808dee385bf91cfe07a06
Chelsea Felix-Shannon <andchelsea@gmail.com>
2017-07-09 15:50:09 -0700

    Merge

--- SeniorProject/Assets/Scripts/EnemyScript.cs
+++ SeniorProject/Assets/Scripts/EnemyScript.cs
@@ -4,13 +4,15 @@ using System.Collections;
 public class EnemyScript : MonoBehaviour
 {
     private float Lifetime = 3;//needed?
+    private float ChildLife = 0;
+    [SerializeField]
+    private GameObject child;
+    [SerializeField] private float hologramLife = 2;
     [SerializeField] private float life = 0;
     [SerializeField] private Shader unlit;//make a shader
     [SerializeField] private Shader lit;//make a shader
     Transform PlayerPos;
     Rigidbody rb;
-    AudioSource DeathSound;
-    ParticleSystem PS;
     // Use this for initialization
     void Awake ()
     {
@@ -18,9 +20,8 @@ public class EnemyScript : MonoBehaviour
         Lifetime = life;
         PlayerPos = VrPlayer.Instance.gameObject.transform;
         rb = GetComponent<Rigidbody>();
-        DeathSound = GetComponent<AudioSource>();
-
-        PS = GetComponent<ParticleSystem>();
+        child = this.gameObject.transform.GetChild(0).gameObject;//enemy structure dependednt
+        child.SetActive(false);
     }
 
 	public void Reset()
@@ -35,23 +36,36 @@ public class EnemyScript : MonoBehaviour
     {
         if (Manager.Instance.IsUpdatable())
         { 
+            if(child.activeSelf)
+            {
+                ChildLife -= Time.deltaTime;
+                if (ChildLife < 0.0f)
+                {
+                   child.SetActive(false);//capture the child in awake
+                }
+            }
+
+
             Lifetime -= Time.deltaTime;
             if (Lifetime < 0.0f)
-                this.gameObject.SetActive(false);//will this work??
+            {
+                child.SetActive(false);
+                this.gameObject.SetActive(false);
+            }
 
             transform.LookAt(PlayerPos);
             rb.velocity = rb.velocity.magnitude * transform.forward;
         }
     }
 
-    void OnMouseExit()//doesnt work
+    void OnMouseExit()
     {
-        GetComponent<Material>().shader = unlit;
+        //GetComponent<Material>().shader = unlit;
     }
 
-    void OnMouseEnter()//doesnt work
+    void OnMouseEnter()
     {
-        GetComponent<Material>().shader = lit;//testing required
+        //GetComponent<Material>().shader = lit;//testing required
     }
 
     void OnParticleCollision(GameObject other)
@@ -63,27 +77,25 @@ public class EnemyScript : MonoBehaviour
     {
         if (other.gameObject.tag == "Player")
         {
+            child.SetActive(false);
             this.gameObject.SetActive(false);
-     
 
             if (--VrPlayer.Instance.Health <= 0)
             {
                 Manager.Instance.SetUpdatable(false);
             }
         }
-    
         else if(other.gameObject.tag == "Bullet")
         {
-            DeathSound.Play();
-            PS.Play();
+            child.SetActive(false);
             other.gameObject.SetActive(false);
             this.gameObject.SetActive(false);
         }
+        else if(other.gameObject.tag == "Flare")
+        {
+            child.SetActive(true);
+            ChildLife = hologramLife;
+            Debug.Log("Enemy hit a Flare!");
+        }
     }
-     
-    void OnFlareDetection()
-    {
-        //this.GetComponentInChildren
-    }
-
 }
